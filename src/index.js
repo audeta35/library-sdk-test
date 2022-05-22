89cf84d8b2baa4e153588ba744c062fa582da02e
cdl <git@citra.digital>
2022-05-22 22:46:19 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import * as React from 'react';
 
 import SessionManagement from './SessionManagement'
+import KeyManagement from './KeyManagement'
 
 export async function multiply(a, b) {
   return new Promise.resolve(a * b);
 }
 
-export function Session() {
+export function Sessions() {
   return (<SessionManagement />);
 }
 
-export function Key () {
-
+export function Keys () {
+  return (<KeyManagement />);
 }
